c8f066f733fb2ed04ac274819b7270d7cee22801
JonyDuarte <joaoc84duarte@gmail.com>
2024-09-24 21:23:11 -0300

    versão1.3

--- app.py
+++ app.py
@@ -5,9 +5,5 @@ peso = float(input("Informe seu peso atual: "))
 # Autorização de entrada com base nas informações
 if idade >= 10 and peso < 150:
     print(f"Entrada autorizada.")
-elif idade < 10:
-    print(f"Não autorizada entrada de menores de 10 anos.")
-elif peso < 150:
-    print(f"Entrada autorizada.")
 else:
-    print(f"Não autorizada entrada a partir de 150 kg.")
+    print(f"Entrada não autorizada.")
