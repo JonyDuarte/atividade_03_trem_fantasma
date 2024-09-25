f55b50ae857eb938b4b560544cb0d7a3f99b9487
JonyDuarte <joaoc84duarte@gmail.com>
2024-09-24 21:21:28 -0300

    versão1.2

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-'''Crie um programa que controle a entrada de usuários no brinquedo "Trem Fantasma"
- de um parque de diversões. O programa só deve autorizar a entrada de usuários que 
- possuam mais de 10 anos de idade e menos de 150kg de peso. 
- Ao terminar, envi para o GitHub'''
-
 # Entrada de dados do usuário
 idade = int(input("Informe sua idade: "))
 peso = float(input("Informe seu peso atual: "))
 
-# Autorização com base nas informações
+# Autorização de entrada com base nas informações
 if idade >= 10 and peso < 150:
-    print(f"Entrada liberada.")
+    print(f"Entrada autorizada.")
 elif idade < 10:
     print(f"Não autorizada entrada de menores de 10 anos.")
 elif peso < 150:
